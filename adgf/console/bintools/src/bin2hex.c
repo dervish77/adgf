b5e7b31e1d2fbdfca443bd90dc01ebd09b91c89f
dervish77 <bslingard@yahoo.com>
2025-08-30 20:54:13 -0400

    formatted the c code

--- adgf/console/bintools/src/bin2hex.c
+++ adgf/console/bintools/src/bin2hex.c
@@ -21,37 +21,34 @@
  *****************************************************************************/
 
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
 
 
 #define DEBUG 0
-
 
 #ifndef TRUE
 #define TRUE 1
 #define FALSE 0
 #endif
 
-
 typedef char BOOLEAN_T;
 
-#define DEFAULT_BYTES_PER_REC	16
-#define MAX_BYTES_PER_REC		64
+#define DEFAULT_BYTES_PER_REC 16
+#define MAX_BYTES_PER_REC 64
 
-#define TYPE_DAT	0
-#define TYPE_EOF	1
-#define TYPE_ESA	2
-#define TYPE_SSA	3
-#define TYPE_ELA	4
-#define TYPE_SLA	5
-
+#define TYPE_DAT 0
+#define TYPE_EOF 1
+#define TYPE_ESA 2
+#define TYPE_SSA 3
+#define TYPE_ELA 4
+#define TYPE_SLA 5
 
 /*  define global variables
  */
-int		base = 0;
-
-
+int base = 0;
 
 /*  ConvertData	- copy binary data to appended file 
  *
@@ -63,72 +60,70 @@ int		base = 0;
  *  RETURNS:
  *	count		- number of bytes copied
  */
-int 
-ConvertData(FILE *in_file_p, FILE *out_file_p, int start, int bytes_rec)
+int
+ConvertData(FILE * in_file_p, FILE * out_file_p, int start, int bytes_rec) 
 {
-   int i;
-   int count = 0;
-   int bytes = 0;
-   int address = start;
-   
-   unsigned char sum = 0;
-   unsigned char c;
-   unsigned char checksum = 0;
+    int i;
+    int count = 0;
+    int bytes = 0;
+    int address = start;
 
-	// hex format -- :bbaaaatt[data]cc
-	// bb - bytes in record
-	// aaaa - address
-	// tt - record type
-	// cc - checksum
+    unsigned char sum = 0;
+    unsigned char c;
+    unsigned char checksum = 0;
 
-   fseek(in_file_p, 0L, SEEK_SET);
+    // hex format -- :bbaaaatt[data]cc
+    // bb - bytes in record
+    // aaaa - address
+    // tt - record type
+    // cc - checksum
 
-   while(!feof(in_file_p)) 
-   {
-	   // copy data for current record
-	   for (i = 0; i < bytes_rec; i++)
-	   {
-		    c = fgetc(in_file_p);
-		    if (!feof(in_file_p))
-		    {	   
-				if (i == 0)
+    fseek(in_file_p, 0L, SEEK_SET);
+
+    while (!feof(in_file_p)) 
+	{
+        // copy data for current record
+        for (i = 0; i < bytes_rec; i++) 
+		{
+            c = fgetc(in_file_p);
+            if (!feof(in_file_p)) 
+			{
+                if (i == 0) 
 				{
-					if (DEBUG) printf("start rec addr %d\n", address);
-					// start new record
-					fprintf(out_file_p, ":%02X", bytes_rec);
-					fprintf(out_file_p, "%04X", address);
-					fprintf(out_file_p, "%02X", TYPE_DAT);
-				}
+                    if (DEBUG) printf("start rec addr %d\n", address);
+                    // start new record
+                    fprintf(out_file_p, ":%02X", bytes_rec);
+                    fprintf(out_file_p, "%04X", address);
+                    fprintf(out_file_p, "%02X", TYPE_DAT);
+                }
 
+                fprintf(out_file_p, "%02X", c);
+                sum += c;
+                address++;
+                count++;
+            }
+        }
 
-				fprintf(out_file_p, "%02X", c);
-				sum += c;
-				address++;
-				count++;
-			}
-	   }
-	   
-	   if (!feof(in_file_p))
-	   {
-			// end of current record
-			if (DEBUG) printf("..end rec count %d sum %02x\n", count, sum);
-			checksum = (~sum) + 1;  // calculate checksum as 2's complement
-			fprintf(out_file_p,"%02X", checksum);
-			fprintf(out_file_p,"\n");
-			sum = 0;
-	   }
-   }
+        if (!feof(in_file_p)) 
+		{
+            // end of current record
+            if (DEBUG) printf("..end rec count %d sum %02x\n", count, sum);
+            checksum = (~sum) + 1; // calculate checksum as 2's complement
+            fprintf(out_file_p, "%02X", checksum);
+            fprintf(out_file_p, "\n");
+            sum = 0;
+        }
+    }
 
-   // end of file
-   fprintf(out_file_p, ":%02X", 0);
-   fprintf(out_file_p, "%04X", 0);
-   fprintf(out_file_p, "%02X", TYPE_EOF);
-   fprintf(out_file_p, "%02X", 255);
-   fprintf(out_file_p,"\n");
+    // end of file
+    fprintf(out_file_p, ":%02X", 0);
+    fprintf(out_file_p, "%04X", 0);
+    fprintf(out_file_p, "%02X", TYPE_EOF);
+    fprintf(out_file_p, "%02X", 255);
+    fprintf(out_file_p, "\n");
 
-   return(count);
+    return (count);
 }
-
 
 /*  main	- main program
  *
@@ -139,125 +134,123 @@ ConvertData(FILE *in_file_p, FILE *out_file_p, int start, int bytes_rec)
  *  RETURNS:
  *	none
  */
-int 
-main(int argc, char **argv)
+int
+main(int argc, char ** argv) 
 {
-   int		i, data = 0, size = 0, count;
+    int i, data = 0, size = 0, count;
 
-   char		input_file_name[40];	/* buffer */
-   char		output_file_name[40];	/* buffer */
+    char input_file_name[40]; /* buffer */
+    char output_file_name[40]; /* buffer */
 
-   FILE		*in_file_p;
-   FILE		*out_file_p;
+    FILE * in_file_p;
+    FILE * out_file_p;
 
-   int		bytes_per_rec = DEFAULT_BYTES_PER_REC;
-   int		start_addr = 0;
+    int bytes_per_rec = DEFAULT_BYTES_PER_REC;
+    int start_addr = 0;
 
-   BOOLEAN_T	verbose = TRUE;
+    BOOLEAN_T verbose = TRUE;
 
-   switch (argc)
-   {
-      case 1:
-         fprintf(stderr, 
-		"Usage:  %s [-h] [-a addr] [-r NN] [-i file] [-o file] [-s]\n", argv[0]);
-         exit(1);
+    switch (argc) 
+	{
+    case 1:
+        fprintf(stderr,
+            "Usage:  %s [-h] [-a addr] [-r NN] [-i file] [-o file] [-s]\n", argv[0]);
+        exit(1);
 
-      default:
-         i = 1;
-         while (i < argc)
-         {
-            if (argv[i][0] == '-')
-            {
-               switch(argv[i][1])
-               {
-                  case 'h':
-                     fprintf(stderr, 
-			"Usage:  %s [-h] [-a addr] [-r NN] [-i file] [-o file] [-s]\n", 
-			argv[0]);
-                     fprintf(stderr,"    -h        get help\n");
-                     fprintf(stderr,"    -a addr   starting address\n");
-                     fprintf(stderr,"    -r NN     bytes per record\n");
-                     fprintf(stderr,"    -i file   input file name\n");
-                     fprintf(stderr,"    -o file   output file name\n");
-                     fprintf(stderr,"    -s        suppress verbose output\n");
-                     if (argc == 2)
+    default:
+        i = 1;
+        while (i < argc) 
+		{
+            if (argv[i][0] == '-') 
+			{
+                switch (argv[i][1]) 
+				{
+                case 'h':
+                    fprintf(stderr,
+                        "Usage:  %s [-h] [-a addr] [-r NN] [-i file] [-o file] [-s]\n",
+                        argv[0]);
+                    fprintf(stderr, "    -h        get help\n");
+                    fprintf(stderr, "    -a addr   starting address\n");
+                    fprintf(stderr, "    -r NN     bytes per record\n");
+                    fprintf(stderr, "    -i file   input file name\n");
+                    fprintf(stderr, "    -o file   output file name\n");
+                    fprintf(stderr, "    -s        suppress verbose output\n");
+                    if (argc == 2)
                         exit(0);
-                     else
+                    else
                         break;
 
-                  case 'a':
-                     start_addr = atoi(argv[i+1]);
-                     i++;
-                     break;
+                case 'a':
+                    start_addr = atoi(argv[i + 1]);
+                    i++;
+                    break;
 
-                  case 'r':
-                     bytes_per_rec = atoi(argv[i+1]);
-                     i++;
-                     break;
-					 
-                  case 'i':
-                     strcpy(input_file_name, argv[i+1]);
-                     i++;
-                     break;
+                case 'r':
+                    bytes_per_rec = atoi(argv[i + 1]);
+                    i++;
+                    break;
 
-                  case 'o':
-                     strcpy(output_file_name, argv[i+1]);
-                     i++;
-                     break;
+                case 'i':
+                    strcpy(input_file_name, argv[i + 1]);
+                    i++;
+                    break;
 
-                  case 's':
-                     verbose = FALSE;
-                     break;
+                case 'o':
+                    strcpy(output_file_name, argv[i + 1]);
+                    i++;
+                    break;
 
-                  default:
-                     fprintf(stderr,
-			"Usage:  %s [-h] [-a addr] [-r NN] [-i file] [-o file] [-s]\n", 
-			argv[0]);
-                     exit(1);
-               }	/* end of inner switch */
+                case 's':
+                    verbose = FALSE;
+                    break;
+
+                default:
+                    fprintf(stderr,
+                        "Usage:  %s [-h] [-a addr] [-r NN] [-i file] [-o file] [-s]\n",
+                        argv[0]);
+                    exit(1);
+                } /* end of inner switch */
+            } else 
+			{
+                fprintf(stderr,
+                    "Usage:  %s [-h] [-a addr] [-r NN] [-i file] [-o file] [-s]\n",
+                    argv[0]);
+                exit(1);
             }
-            else
-            {
-               fprintf(stderr,
-			"Usage:  %s [-h] [-a addr] [-r NN] [-i file] [-o file] [-s]\n", 
-			argv[0]);
-               exit(1);
-            }
 
-            i++;	/* increment arg index */
-         } 	/* end while */
-         break;
-   } /* end of outer switch */
+            i++; /* increment arg index */
+        } /* end while */
+        break;
+    } /* end of outer switch */
 
-   printf("\nbin2hex -- Converting binary to hex file\n");
+    printf("\nbin2hex -- Converting binary to hex file\n");
 
-   if (verbose) printf("bin2hex -- Opening binary file '%s' ...\n", input_file_name);
+    if (verbose) printf("bin2hex -- Opening binary file '%s' ...\n", input_file_name);
 
-   in_file_p = fopen(input_file_name, "r");
-   if (in_file_p == (FILE *)NULL)
-   {
-      fprintf(stderr, "Couldn't open %s as input\n", input_file_name);
-      exit(1);
-   }
+    in_file_p = fopen(input_file_name, "r");
+    if (in_file_p == (FILE * ) NULL) 
+	{
+        fprintf(stderr, "Couldn't open %s as input\n", input_file_name);
+        exit(1);
+    }
 
-   if (verbose) printf("bin2hex -- Opening output file '%s' ...\n", output_file_name);
+    if (verbose) printf("bin2hex -- Opening output file '%s' ...\n", output_file_name);
 
-   out_file_p = fopen(output_file_name, "w");
-   if (out_file_p == (FILE *)NULL)
-   {
-      fprintf(stderr, "Couldn't open %s as output\n", output_file_name);
-      exit(1);
-   }
+    out_file_p = fopen(output_file_name, "w");
+    if (out_file_p == (FILE * ) NULL) 
+	{
+        fprintf(stderr, "Couldn't open %s as output\n", output_file_name);
+        exit(1);
+    }
 
-   count = ConvertData(in_file_p, out_file_p, start_addr, bytes_per_rec);
+    count = ConvertData(in_file_p, out_file_p, start_addr, bytes_per_rec);
 
-   if (verbose) printf("bin2hex -- converted %d bytes from binary to hex\n\n", count);
+    if (verbose) printf("bin2hex -- converted %d bytes from binary to hex\n\n", count);
 
-   fclose(in_file_p);
-   fclose(out_file_p);
+    fclose(in_file_p);
+    fclose(out_file_p);
 
-   exit(0);
+    exit(0);
 }
-
 
 /* end of bin2hex.c */
--- adgf/console/bintools/src/bingen.c
+++ adgf/console/bintools/src/bingen.c
@@ -22,7 +22,9 @@
  *****************************************************************************/
 
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
 
 
@@ -31,17 +33,16 @@
 #define FALSE 0
 #endif
 
-
 typedef char BOOLEAN_T;
 
-#define MODE_ALPHA	1
-#define MODE_COUNT	2
-#define MODE_DATA	3
+#define MODE_ALPHA 1
+#define MODE_COUNT 2
+#define MODE_DATA 3
 
 
 /*  define global variables
  */
-int		base = 0;
+int base = 0;
 
 
 /*  GenData	- counts bytes in binary file 
@@ -55,45 +56,44 @@ int		base = 0;
  *  RETURNS:
  *	size		- number of bytes converted
  */
-int 
-GenData(FILE *out_file_p, int size, int data, int mode)
+int
+GenData(FILE * out_file_p, int size, int data, int mode) 
 {
-   char c = -1;
-   int i, count = 0;
+    char c = -1;
+    int i, count = 0;
 
-   if (mode == MODE_ALPHA)
-      c = 0x40;
+    if (mode == MODE_ALPHA)
+        c = 0x40;
 
-   for (i = 0; i < size; i++) 
-   {
-      switch(mode)
-      {
-         case MODE_ALPHA:
+    for (i = 0; i < size; i++) 
+	{
+        switch (mode) 
+		{
+        case MODE_ALPHA:
             c++;
-            if ( c > 'Z' ) c = 0x40;
+            if (c > 'Z') c = 0x40;
             break;
 
-         case MODE_COUNT:
+        case MODE_COUNT:
             c++;
             break;
 
-         case MODE_DATA:
+        case MODE_DATA:
             c = (char) data;
             break;
 
-         default:
+        default:
             fprintf(stderr, "Unknown data mode\n");
-            exit(1); 
+            exit(1);
             break;
-      }
+        }
 
-      fputc(c, out_file_p);
-      count++;
-   }
+        fputc(c, out_file_p);
+        count++;
+    }
 
-   return(count);
+    return (count);
 }
-
 
 /*  main	- main program
  *
@@ -104,117 +104,115 @@ GenData(FILE *out_file_p, int size, int data, int mode)
  *  RETURNS:
  *	none
  */
-int 
-main(int argc, char **argv)
+int
+main(int argc, char ** argv) 
 {
-   int		i, mode = MODE_DATA, data = 0, size = 0, count;
+    int i, mode = MODE_DATA, data = 0, size = 0, count;
 
-   char		output_file_name[40];	/* buffer */
-   char		array_name[40];		/* buffer */
+    char output_file_name[40]; /* buffer */
+    char array_name[40]; /* buffer */
 
-   FILE		*out_file_p;
+    FILE * out_file_p;
 
-   BOOLEAN_T	verbose = TRUE;
+    BOOLEAN_T verbose = TRUE;
 
-   switch (argc)
-   {
-      case 1:
-         fprintf(stderr, 
-		"Usage:  %s [-h] [-n bytes] [-a | -c | -d data] [-o file] [-s]\n", argv[0]);
-         exit(1);
+    switch (argc) 
+	{
+    case 1:
+        fprintf(stderr,
+            "Usage:  %s [-h] [-n bytes] [-a | -c | -d data] [-o file] [-s]\n", argv[0]);
+        exit(1);
 
-      default:
-         i = 1;
-         while (i < argc)
-         {
-            if (argv[i][0] == '-')
-            {
-               switch(argv[i][1])
-               {
-                  case 'a':
-                     mode = MODE_ALPHA;
-                     break;
+    default:
+        i = 1;
+        while (i < argc) 
+		{
+            if (argv[i][0] == '-') 
+			{
+                switch (argv[i][1]) 
+				{
+                case 'a':
+                    mode = MODE_ALPHA;
+                    break;
 
-                  case 'c':
-                     mode = MODE_COUNT;
-                     break;
+                case 'c':
+                    mode = MODE_COUNT;
+                    break;
 
-                  case 'd':
-                     data = atoi(argv[i+1]);
-                     mode = MODE_DATA;
-                     i++;
-                     break;
+                case 'd':
+                    data = atoi(argv[i + 1]);
+                    mode = MODE_DATA;
+                    i++;
+                    break;
 
-                  case 'h':
-                     fprintf(stderr, 
-			"Usage:  %s [-h] [-n bytes] [-a | -c | -d data] [-o file] [-s]\n", 
-			argv[0]);
-                     fprintf(stderr,"    -h        get help\n");
-                     fprintf(stderr,"    -n bytes  num bytes to generate\n");
-                     fprintf(stderr,"    -a        alpha data\n");
-                     fprintf(stderr,"    -c        counting data\n");
-                     fprintf(stderr,"    -d data   binary data\n");
-                     fprintf(stderr,"    -o file   output file name\n");
-                     fprintf(stderr,"    -s        suppress verbose output\n");
-                     if (argc == 2)
+                case 'h':
+                    fprintf(stderr,
+                        "Usage:  %s [-h] [-n bytes] [-a | -c | -d data] [-o file] [-s]\n",
+                        argv[0]);
+                    fprintf(stderr, "    -h        get help\n");
+                    fprintf(stderr, "    -n bytes  num bytes to generate\n");
+                    fprintf(stderr, "    -a        alpha data\n");
+                    fprintf(stderr, "    -c        counting data\n");
+                    fprintf(stderr, "    -d data   binary data\n");
+                    fprintf(stderr, "    -o file   output file name\n");
+                    fprintf(stderr, "    -s        suppress verbose output\n");
+                    if (argc == 2)
                         exit(0);
-                     else
+                    else
                         break;
 
-                  case 'o':
-                     strcpy(output_file_name, argv[i+1]);
-                     i++;
-                     break;
+                case 'o':
+                    strcpy(output_file_name, argv[i + 1]);
+                    i++;
+                    break;
 
-                  case 'n':
-                     size = atoi(argv[i+1]);
-                     i++;
-                     break;
+                case 'n':
+                    size = atoi(argv[i + 1]);
+                    i++;
+                    break;
 
-                  case 's':
-                     verbose = FALSE;
-                     break;
+                case 's':
+                    verbose = FALSE;
+                    break;
 
-                  default:
-                     fprintf(stderr,
-			"Usage: %s [-h] [-n bytes] [-d data] [-o file]  [-s]\n",
-			argv[0]);
-                     exit(1);
-               }	/* end of inner switch */
+                default:
+                    fprintf(stderr,
+                        "Usage: %s [-h] [-n bytes] [-d data] [-o file]  [-s]\n",
+                        argv[0]);
+                    exit(1);
+                } /* end of inner switch */
+            } else 
+			{
+                fprintf(stderr,
+                    "Usage: %s [-h] [-n bytes] [-d data] [-o file]  [-s]\n",
+                    argv[0]);
+                exit(1);
             }
-            else
-            {
-               fprintf(stderr,
-			"Usage: %s [-h] [-n bytes] [-d data] [-o file]  [-s]\n",
-			argv[0]);
-               exit(1);
-            }
 
-            i++;	/* increment arg index */
-         } 	/* end while */
-         break;
-   } /* end of outer switch */
+            i++; /* increment arg index */
+        } /* end while */
+        break;
+    } /* end of outer switch */
 
-   printf("\nbingen -- Generating binary file with %d bytes of 0x%02x\n",
-	size, data );
+    printf("\nbingen -- Generating binary file with %d bytes of 0x%02x\n",
+        size, data);
 
-   if (verbose) printf("bingen -- opening output file '%s' ...\n", output_file_name);
+    if (verbose) printf("bingen -- opening output file '%s' ...\n", output_file_name);
 
-   out_file_p = fopen(output_file_name, "w");
-   if (out_file_p == (FILE *)NULL)
-   {
-      fprintf(stderr, "Couldn't open %s as output\n", output_file_name);
-      exit(1);
-   }
+    out_file_p = fopen(output_file_name, "w");
+    if (out_file_p == (FILE * ) NULL) 
+	{
+        fprintf(stderr, "Couldn't open %s as output\n", output_file_name);
+        exit(1);
+    }
 
-   count = GenData(out_file_p, size, data, mode);
+    count = GenData(out_file_p, size, data, mode);
 
-   if (verbose) printf("bingen -- generated %d bytes\n\n", count);
+    if (verbose) printf("bingen -- generated %d bytes\n\n", count);
 
-   fclose(out_file_p);
+    fclose(out_file_p);
 
-   exit(0);
+    exit(0);
 }
-
 
 /* end of bingen.c */
