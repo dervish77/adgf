22abe2244f36f5c702c2d52150826fa8d6ff0aef
dervish77 <bslingard@yahoo.com>
2025-09-05 12:03:45 -0400

    modified to include query to run again

--- dev/c64/c/fibo/main.c
+++ dev/c64/c/fibo/main.c
@@ -12,6 +12,8 @@
 int main( void )
 {	
 	int results = 0;
+	int done = 0;
+	char answer[4];
 	
 #ifdef USE_COLOR
 	POKE( 0xd020, 13 ); /* set border to green */
@@ -19,20 +21,31 @@ int main( void )
 	POKE( 646, 1 );   /* set text to white */
 #endif	
 
-	printf("### Fibonacci C64 ###\n");
-	printf("\n");
-	printf("- how many results? ");
-	scanf("%d", &results );
-	
-	if ( results > MINCOUNT && results <= MAXCOUNT )
+	while (!done)
 	{
+		printf("\n### Fibonacci C64 ###\n");
 		printf("\n");
-		do_fib( results );
-		printf("\n");
-	}
-	else
-	{
-		printf("ERROR: enter number between %d and %d\n", MINCOUNT, MAXCOUNT );
+		printf("- how many results? ");
+		scanf("%d", &results );
+		
+		if ( results > MINCOUNT && results <= MAXCOUNT )
+		{
+			printf("\n");
+			do_fib( results );
+			printf("\n");
+		}
+		else
+		{
+			printf("ERROR: enter number between %d and %d\n", MINCOUNT, MAXCOUNT );
+			done = 1;
+		}
+		
+		if (!done)
+		{
+			printf("Run again (y/n)?");
+			scanf("%s", &answer);
+			if (answer[0] == 'n') done = 1;
+		}
 	}
     
     return EXIT_SUCCESS;
